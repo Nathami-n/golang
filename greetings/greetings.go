f76c42230f748a5585e46c1366ad6b341642751f
Nathami-n <georgenathan010@gmail.com>
2024-08-07 19:08:41 +0300

    final commit for the day

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,28 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi %v", name)
+func Hello(name string) (string, error) {
+	if name == "" {
 
-	return message
+		return "", errors.New("no name specified")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+
+}
+
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
