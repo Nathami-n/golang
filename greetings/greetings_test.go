87d064d3f53fc0c3cdafab61858a1c391351b3ce
Nathami-n <georgenathan010@gmail.com>
2024-08-07 20:51:18 +0300

    add: go command

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -18,3 +18,9 @@ func TestHelloName(t *testing.T) {
 	}
 }
 
+func TestHelloEmpty(t *testing.T) {
+    msg, err := Hello("")
+    if msg != "" || err == nil {
+        t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+    }
+}
